ea2669cdbd8c8c2e0d61df2be4d8fd6724e009f5
Faraz <msharif42@gmail.com>
2020-03-23 18:41:07 -0500

    Update freeze.py

--- freeze.py
+++ freeze.py
@@ -1,1 +1,63 @@
+"""
+ @file
+ @brief cx_Freeze script to build Shvideo package with dependencies (for Mac and Windows)
+ @author Mudassir Faraz sharif <msharif42@gmail.com>
+ @section LICENSE
+ Copyright (c) 2020-2028 Shvideos Studios, LLC
+ This file is part of
+ Shvideo Video Editor, an open-source project
+ delivering video editing and animation answers
+ to the world.
+ Shvideo Video Editor is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ Shvideo Video Editor is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ You should have received a copy of the GNU General Public License
+ along with Shvideo Library.  If not, see <http://www.gnu.org/licenses/>.
+ """
 
+# Syntax to build redistributable package:  python3 freeze.py build
+#
+# Troubleshooting: If you encounter an error while attempting to freeze
+# the PyQt5/uic/port_v2, remove the __init__.py in that folder. And if
+# you are manually compiling PyQt5 on Windows, remove the -strip line
+# from the Makefile. On Mac, just delete the port_v2 folder. Also, you
+# might need to remove the QtTest.so from /usr/local/lib/python3.3/site-packages/PyQt5,
+# if you get errors while freezing.
+#
+# Mac Syntax to Build App Bundle:
+# 1) python3 freeze.py bdist_mac --qt-menu-nib="/usr/local/Cellar/qt5/5.4.2/plugins/platforms/" --iconfile=installer/shvideo.icns --custom-info-plist=installer/Info.plist --bundle-name="Shvideo Video Editor"
+# 2) change Contents/Info.plist to use launch-mac.sh as the Executable name
+# 3) manually fix rsvg executable:
+#    sudo dylibbundler -od -of -b -x ~/apps/rsvg/rsvg-convert -d ./rsvg-libs/ -p @executable_path/rsvg-libs/
+# 4) Code sign and create the DMG (disk image)
+#    a) cd ~/apps/shvideo-qt-git/
+#    b) bash installer/build-mac-dmg.sh
+#
+# Windows Syntax to Build MSI Installer
+# NOTE: Python3.5 requires custom build of cx_Freeze (https://github.com/sekrause/cx_Freeze-wheels). Download, python setup.py build, python setup.py install
+# 1) python3 freeze.py bdist_msi
+# NOTE: Requires a tweak to cx_freeze: http://stackoverflow.com/questions/24195311/how-to-set-shortcut-working-directory-in-cx-freeze-msi-bundle
+# 2) Sign MSI with private code signing key (optional)
+#  NOTE: Install Windows 10 SDK first
+#  signtool sign /v /f OSStudiosSPC.pfx "shvideo Video Editor-2.0.0-win32.msi"
+
+import inspect 
+import os
+import sys
+import fnmatch
+from shutil import copytree, rmtree, copy 
+from cx_Freeze import setup, Executable 
+import cx_Freeze 
+from PyQt5.QtCore import QLibraryInfo 
+import shutil 
+
+
+print (str(cx_Freeze))
+
+#set '${ARCHLIB}' envvar to override system library path
+ARCHLIB =
